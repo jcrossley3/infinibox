57c8c2a98ecf4f1b56a3ac4d569ae9b4ab40f8a4
penumbra <penumbraposts@yahoo.com>
2011-06-24 13:34:00 -0400

    scripts to start each node on localhost

---
+++ torquebox/jboss/bin/run1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./run.sh -c node1 -Djboss.service.binding.set=ports-01 -Djboss.messaging.ServerPeerID=1
---
+++ torquebox/jboss/bin/run2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./run.sh -c node2 -Djboss.service.binding.set=ports-02 -Djboss.messaging.ServerPeerID=2
