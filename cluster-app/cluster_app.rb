0d89ea4515d142994c1e7fed34fd7ed9d0fa33ba
penumbra <penumbraposts@yahoo.com>
2011-06-26 02:58:48 -0400

    use yaml for rack app config

--- cluster-app/cluster_app.rb
+++ cluster-app/cluster_app.rb
@@ -1,21 +1,32 @@
 require 'rubygems'
 require 'rack'
+require 'yaml'
 require 'jmx'
+
 require 'torquebox'
 
 class ClusterApp
   java_import javax.management.ObjectName
 
   def initialize
-    @jmx_server ||= JMX::MBeanServer.new
+    options = YAML::load( File.open( File.join('config', 'cluster-app.yml') ))
+    jmx_server ||= JMX::MBeanServer.new
 
     @logger = TorqueBox::Logger.new( self.class )
+
+    jmx_svc_lookup = options[:jmx_svc_lookup] 
+    jmx_as_lookup = options[:jmx_as_lookup]
+    
+    @jboss_as = jmx_server[javax.management.ObjectName.new( jmx_as_lookup)]
+    @jboss_svc = jmx_server[javax.management.ObjectName.new( jmx_svc_lookup )]
   end
 
   def call(env)
-    jboss_as = @jmx_server[javax.management.ObjectName.new( 'jboss.system:type=ServerConfig')]
-    jboss_svc = @jmx_server[javax.management.ObjectName.new( 'jboss.system:service=ServiceBindingManager')]
+    sn = @jboss_as.server_name
+    svn = @jboss_svc.server_name
 
-    [200, {"Content-Type" => "text/plain"}, ["JBossAS 6 ServerName => #{jboss_as.server_name}, binding => #{jboss_svc.server_name}"]]
+    @logger.info "scheduler activated #{self.class.to_s} on node #{sn}"
+
+    [200, {"Content-Type" => "text/plain"}, ["JBossAS 6 ServerName => #{sn}, binding => #{svn}"]]
   end
 end
