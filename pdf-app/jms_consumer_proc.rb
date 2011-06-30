499642973f6b66e86c754e0ba87c29606a748cc0
penumbra <penumbraposts@yahoo.com>
2011-06-29 22:27:14 -0400

    added file write

--- pdf-app/jms_consumer_proc.rb
+++ pdf-app/jms_consumer_proc.rb
@@ -7,11 +7,20 @@ class JmsConsumerProc < TorqueBox::Messaging::MessageProcessor
   end
 
   def on_message(msg)
+    pdf_doc = nil
+
     if msg.kind_of? Hash
       pdf_doc = msg[:msg]
-      @logger.info "received hash message => #{pdf_doc.size} bytes"
+      fn = File.join('public', 'hash-torquebox-doc.pdf')
     else
-      @logger.info "received raw data => #{msg.size} bytes"
+      pdf_doc = msg
+      fn = File.join('public', 'raw-torquebox-doc.pdf')
     end
+
+    @logger.info "received message => #{pdf_doc.size} bytes"
+
+    f = File.new(fn, 'wb')
+    f.write( pdf_doc )
+    f.close
   end
 end
