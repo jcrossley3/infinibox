f7527e4745e82e78b0b0318a8de0bcd6ce42ca44
penumbra <penumbraposts@yahoo.com>
2011-07-08 04:10:50 -0400

    updated to use torquebox_home from env

--- hornetq/deploy-hornetq.rb
+++ hornetq/deploy-hornetq.rb
@@ -3,27 +3,29 @@
 require 'rubygems'
 require 'fileutils'
 
+home = ENV['TORQUEBOX_HOME']
+
 src = 'build/jars/hornetq-core.jar'
-dest0 = '/projects/torquebox/jboss/common/lib/hornetq-core.jar'
+dest0 = home + '/jboss/common/lib/hornetq-core.jar'
 FileUtils::copy src, dest0
 
 src = 'build/jars/hornetq-core.jar'
-dest1 = '/projects/torquebox/jruby/lib/ruby/gems/1.8/gems/torquebox-messaging-1.0.2-java/lib/hornetq-core-2.1.2.Final.jar'
+dest1 = home + '/jruby/lib/ruby/gems/1.8/gems/torquebox-messaging-1.0.2-java/lib/hornetq-core-2.1.2.Final.jar'
 FileUtils::copy src, dest1
 
 src = 'build/jars/hornetq-core.jar'
-dest2 = '/projects/torquebox/jruby/lib/ruby/gems/1.8/gems/torquebox-messaging-container-1.0.2-java/lib/hornetq-core-2.1.2.Final.jar'
+dest2 = home + '/jruby/lib/ruby/gems/1.8/gems/torquebox-messaging-container-1.0.2-java/lib/hornetq-core-2.1.2.Final.jar'
 FileUtils::copy src, dest2
 
 src2 = 'build/jars/hornetq-core-client.jar'
-dest3 = '/projects/torquebox/jboss/client/hornetq-core-client.jar'
+dest3 = home + '/jboss/client/hornetq-core-client.jar'
 FileUtils::copy src2, dest3
 
 
 src2 = 'build/jars/hornetq-core-client.jar'
-dest4 = '/projects/torquebox/jruby/lib/ruby/gems/1.8/gems/torquebox-messaging-1.0.2-java/lib/hornetq-jms-client-2.1.2.Final.jar'
+dest4 = home + '/jruby/lib/ruby/gems/1.8/gems/torquebox-messaging-1.0.2-java/lib/hornetq-jms-client-2.1.2.Final.jar'
 FileUtils::copy src2, dest4
 
 src3 = 'build/jars/hornetq-jms.jar'
-dest5 = '/projects/torquebox/jboss/common/lib/hornetq-jms.jar'
+dest5 = home + '/jboss/common/lib/hornetq-jms.jar'
 
