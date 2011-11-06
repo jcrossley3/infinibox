400ada1d558e8203c4cf3484846a8a438459b598
penumbra <penumbraposts@yahoo.com>
2011-11-06 18:53:42 -0500

    added spec

---
+++ cluster-app/spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'libs.rb'
+require 'rubygems'
+require 'torquespec'
+
+TorqueSpec.configure do |config|
+  config.drb_port = 7772
+  config.knob_root = ".torquespec"
+  config.jboss_home = '/tools/torquebox/jboss'
+  config.lazy = true
+  config.jvm_args = "-Xms64m -Xmx1024m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSClassUnloadingEnabled -Dgem.path=default"
+end
