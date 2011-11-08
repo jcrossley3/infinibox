95faced2337b836d3643f2ea35d23f28c412dcb1
Jim Crossley <jim@crossleys.org>
2011-11-07 23:05:18 -0500

    Use JBOSS_HOME

--- cluster-app/spec/libs.rb
+++ cluster-app/spec/libs.rb
@@ -1,9 +1,9 @@
 require 'java'
 
-$LIB1 = '/tools/torquebox/jboss/modules/org/jboss/logging/main'
-$LIB2 = '/tools/torquebox/jboss/modules/org/infinispan/main/'
-$LIB3 = '/tools/torquebox/jboss/modules/javax/jms/api/main/'
-$LIB4 = '/tools/torquebox/jboss/modules/javax/transaction/api/main'
+$LIB1 = "#{ENV['JBOSS_HOME']}/modules/org/jboss/logging/main"
+$LIB2 = "#{ENV['JBOSS_HOME']}/modules/org/infinispan/main/"
+$LIB3 = "#{ENV['JBOSS_HOME']}/modules/javax/jms/api/main/"
+$LIB4 = "#{ENV['JBOSS_HOME']}/modules/javax/transaction/api/main"
 require File.join($LIB1, 'jboss-logging-3.1.0.Beta3.jar')
 require File.join($LIB2, 'infinispan-core-5.1.0.BETA2.jar')
 require File.join($LIB3, 'jboss-jms-api_1.1_spec-1.0.0.Final.jar')
