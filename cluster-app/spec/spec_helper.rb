1054935c68015f08449c5a38a14e62b126388de2
Jim Crossley <jim@crossleys.org>
2011-11-07 23:01:15 -0500

    We can't have any knobs within knobs or jboss will try to deploy them

--- cluster-app/spec/spec_helper.rb
+++ cluster-app/spec/spec_helper.rb
@@ -4,8 +4,8 @@ require 'torquespec'
 
 TorqueSpec.configure do |config|
   config.drb_port = 7772
-  config.knob_root = ".torquespec"
-  config.jboss_home = '/tools/torquebox/jboss'
+  config.knob_root = "/tmp/.torquespec"
+  # config.jboss_home = '/tools/torquebox/jboss'
   config.lazy = true
   config.jvm_args = "-Xms64m -Xmx1024m -XX:MaxPermSize=512m -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSClassUnloadingEnabled -Dgem.path=default"
 end
